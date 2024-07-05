978d3676c59d15de20d19df398bd8b5aa14a4712
Hinga Peter <hingapeter18@gmail.com>
2024-07-05 13:54:41 +0300

    bug deletion

--- web_technologies_module/week-11/weatherapp/script.js
+++ web_technologies_module/week-11/weatherapp/script.js
@@ -1,3 +1,4 @@
+
 const apiKey = "bd63b41c4c58cd3ab801e871daae6296";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
@@ -8,11 +9,9 @@ const temperatureElement = document.getElementById("temperature");
 const descriptionElement = document.getElementById("description");
 
 searchButton.addEventListener("click", () => {
-    const location = locationInput.value.trim();
+    const location = locationInput.value;
     if (location) {
         fetchWeather(location);
-    } else {
-        alert("Please enter a City name");
     }
 });
 
@@ -20,13 +19,7 @@ function fetchWeather(location) {
     const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
 
     fetch(url)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Weather data not available or invalid city");
-            }
-            return response.json();
-        
-        })
+        .then((response) => response.json())
         .then((data) => {
             locationElement.textContent = data.name;
             temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
